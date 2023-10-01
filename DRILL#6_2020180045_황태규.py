e9b1de109d3e309306285a69a97bd8edfcb3d3b4
TaeGyu-Hwang <hwg4906@gmail.com>
2023-10-01 18:56:40 +0900

    변수 설정

--- DRILL#6_2020180045_황태규.py
+++ DRILL#6_2020180045_황태규.py
@@ -7,7 +7,11 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
+hand_arrow = load_image('hand_arrow.png')
 
+direction = 0  # 0: left, 1: right
+move_speed = 20
+target_queue = deque()  # 마우스 클릭 위치를 저장할 큐
 
 def handle_events():
     global running
